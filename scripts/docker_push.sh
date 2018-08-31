a50940d1a5c85ca3bef30eed22e3bd3579d2ccee
Brandur <brandur@brandur.org>
2018-08-31 15:11:44 -0700

    Create tags for Docker Hub releases
    Creates tags in Docker Hub for (1) the `master` branch when it's built,
    and (2) for any builds for Git tags.

--- scripts/docker_push.sh
+++ scripts/docker_push.sh
@@ -7,4 +7,16 @@
 #     https://docs.travis-ci.com/user/docker/
 
 docker login -u "$DOCKER_USERNAME" -p "$DOCKER_PASSWORD";
+
+# $TRAVIS_TAG contains a tag if this is a build for a tag, otherwise it's
+# empty.
+#
+# Create Docker Hub tags for Git tags and keep one in place for the master
+# branch.
+if [ ! -z "$TRAVIS_TAG" ]; then
+    docker tag "$DOCKER_REPO:${TRAVIS_COMMIT:0:8}" "$DOCKER_REPO:$TRAVIS_TAG"
+elif [ "$TRAVIS_BRANCH" == "master"]; then
+    docker tag "$DOCKER_REPO:${TRAVIS_COMMIT:0:8}" "$DOCKER_REPO:master"
+fi
+
 docker push "$DOCKER_REPO"
