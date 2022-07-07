a059f8e665c0390b6e208996b890225b8c29cf14
pakrym-stripe <99349468+pakrym-stripe@users.noreply.github.com>
2022-07-07 10:37:17 -0700

    Fix additionalProperties loading (#338)

--- spec/spec_test.go
+++ spec/spec_test.go
@@ -15,6 +15,52 @@ func TestUnmarshal_Simple(t *testing.T) {
 	assert.Equal(t, "string", schema.Type)
 }
 
+func TestUnmarshal_ObjectWithAdditionalProperties(t *testing.T) {
+	data := []byte(`{
+		"type": "object",
+		"additionalProperties": {
+			"properties": {
+				"prop": {
+					"type": "number"
+				}
+			},
+			"type": "object"
+		}
+	}`)
+	var schema Schema
+	err := json.Unmarshal(data, &schema)
+	assert.NoError(t, err)
+	assert.Equal(t, "object", schema.Type)
+	assert.True(t, schema.AdditionalPropertiesAllowed)
+	assert.Equal(t, "object", schema.AdditionalProperties.Type)
+	assert.Equal(t, 1, len(schema.AdditionalProperties.Properties))
+}
+
+func TestUnmarshal_ObjectWithAdditionalPropertiesFalse(t *testing.T) {
+	data := []byte(`{
+		"type": "object",
+		"additionalProperties": false
+	}`)
+	var schema Schema
+	err := json.Unmarshal(data, &schema)
+	assert.NoError(t, err)
+	assert.Equal(t, "object", schema.Type)
+	assert.False(t, schema.AdditionalPropertiesAllowed)
+	assert.Nil(t, schema.AdditionalProperties)
+}
+
+func TestUnmarshal_ObjectWithAdditionalPropertiesDefault(t *testing.T) {
+	data := []byte(`{
+		"type": "object"
+	}`)
+	var schema Schema
+	err := json.Unmarshal(data, &schema)
+	assert.NoError(t, err)
+	assert.Equal(t, "object", schema.Type)
+	assert.True(t, schema.AdditionalPropertiesAllowed)
+	assert.Nil(t, schema.AdditionalProperties)
+}
+
 func TestUnmarshal_UnsupportedField(t *testing.T) {
 	// We don't support 'const'
 	data := []byte(`{const: "hello"}`)
